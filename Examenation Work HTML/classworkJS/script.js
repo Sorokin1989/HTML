1307bf67f65f168c5d92a1b149eb1b21e95d3f1b
Dmitrii <sorokindm1try@yandex.ru>
2025-10-08 13:45:12 +0300

    exam 11.0

--- Examenation Work HTML/classworkJS/script.js
+++ Examenation Work HTML/classworkJS/script.js
@@ -1,2 +1,16 @@
 console.log('Hello world')
 
+// var x=1;
+// console.log(x++ - ++x)
+
+var x=true;
+var y=10;
+
+if(!!true|++y>10){
+    console.log('ok');
+    
+}
+
+console.log(x);
+console.log(y);
+
